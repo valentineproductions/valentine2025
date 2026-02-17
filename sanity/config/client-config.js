38ed6a10b1beeb0ea775fae60cb3a93bdbd65808
Keiner <stickoscr@gmail.com>
2026-02-17 12:35:38 -0600

    updates on env changes

--- sanity/config/client-config.js
+++ sanity/config/client-config.js
@@ -1,8 +1,13 @@
+const vercelEnv = process.env.VERCEL_ENV;
+const datasetOverride = (vercelEnv === "preview" || process.env.NODE_ENV === "development")
+  ? "staging"
+  : (process.env.NEXT_PUBLIC_SANITY_DATASET || "production");
+
 const config = {
     
     // Use environment variables for projectId and dataset
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    dataset: datasetOverride,
     
     apiVersion: "2025-05-10", // Mothers's day
     useCdn: false,
